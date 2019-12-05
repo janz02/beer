14b27e9d3ce1f0cb65fe24879bc23d01ed4ef4de
Albert Hajdu <hajdu.albert89@gmail.com>
2019-12-05 16:40:46 +0100

    Refactorings and adding test route

--- src/router.tsx
+++ src/router.tsx
@@ -3,11 +3,13 @@ import { withRouter, Switch } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 import PrivateRoute from 'components/routes/PrivateRoute';
 import Home from 'components/views/Home';
+import Test from 'components/views/Test';
 
 const Routes = () => {
   return (
     <Switch>
       <PrivateRoute exact path="/" component={Home} />
+      <PrivateRoute exact path="/test" component={Test} />
     </Switch>
   );
 };
