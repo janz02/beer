1be5fd78fa007356916a296435676b4d7a31ac44
Albert Hajdu <hajdu.albert89@gmail.com>
2019-12-10 14:02:38 +0100

    Adding thunk typing

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,5 +1,6 @@
-import { configureStore } from '@reduxjs/toolkit';
-import rootReducer from './rootReducer';
+import { configureStore, Action } from '@reduxjs/toolkit';
+import rootReducer, { RootState } from './rootReducer';
+import { ThunkAction } from 'redux-thunk';
 
 const store = configureStore({
   reducer: rootReducer,
@@ -13,5 +14,7 @@ if (process.env.NODE_ENV === 'development' && (module as any).hot) {
 }
 
 export type AppDispatch = typeof store.dispatch;
+
+export type AppThunk = ThunkAction<void, RootState, null, Action<string>>;
 
 export default store;
