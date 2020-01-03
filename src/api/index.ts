3bd7685946d818efe8cdc5d5e87bb0e7b736cb22
Kovács Ádám <kovacs.adam@grape.hu>
2020-01-03 14:29:43 +0100

    Prod build

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,9 +2,7 @@ import { Configuration } from './swagger/runtime';
 import { CouponsApi, CategoriesApi } from './swagger/apis';
 
 const config: Configuration = new Configuration({
-  basePath: process.env.REACT_APP_API_URL
-    ? process.env.REACT_APP_API_URL
-    : 'http://104.199.97.19',
+  basePath: process.env.REACT_APP_API_URL,
   apiKey: () => `Bearer ${'jwt'}`,
 });
 
