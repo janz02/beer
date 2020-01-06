4a45f8dea0b60bca767e0458176494c882778822
Kovács Ádám <kovacs.adam@grape.hu>
2020-01-06 16:32:36 +0100

    Added comment

--- config/generate-client.js
+++ config/generate-client.js
@@ -1,5 +1,6 @@
 const shell = require('shelljs')
 
+// TODO: Use final DNS, lookout for prod - staging differences
 shell.ShellString(shell.exec("curl http://104.199.97.19/swagger/v1/swagger.json")).to("src/api/swagger.json")
 
 shell.exec("openapi-generator generate -i src/api/swagger.json -g typescript-fetch -p typescriptThreePlus=true -o src/api/swagger")
