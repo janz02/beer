d83f399895b4a0de3b63bc344cd0a0d30565f35e
Kovács Ádám <kovacs.adam@grape.hu>
2020-01-06 16:30:11 +0100

    Generate Api from script

---
+++ config/generate-client.js
@@ -0,0 +1,5 @@
+const shell = require('shelljs')
+
+shell.ShellString(shell.exec("curl http://104.199.97.19/swagger/v1/swagger.json")).to("src/api/swagger.json")
+
+shell.exec("openapi-generator generate -i src/api/swagger.json -g typescript-fetch -p typescriptThreePlus=true -o src/api/swagger")
