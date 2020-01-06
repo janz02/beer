bf33c230763a8444de7bd67e4f4b3d4facc760cf
Kovács Ádám <kovacs.adam@grape.hu>
2020-01-06 14:25:08 +0100

    Rewritten back to react-app-reired for tests, and added setupfile and configs to it

--- config/setupTests.ts
+++ config/setupTests.ts
@@ -1,4 +1,4 @@
 import { configure } from "enzyme";
-import { default as Adapter } from "enzyme-adapter-react-16";
+import Adapter from "enzyme-adapter-react-16";
 
 configure({ adapter: new Adapter() });
