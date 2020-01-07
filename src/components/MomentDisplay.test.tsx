bbdad25ad8dd9478cd3c8e638b47fa9835e142f9
Kovács Ádám <kovacs.adam@grape.hu>
2020-01-07 13:52:27 +0100

    MomentDisplay tests

--- src/components/MomentDisplay.test.tsx
+++ src/components/MomentDisplay.test.tsx
@@ -1,9 +1,36 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import MomentDisplay from './MomentDisplay';
+import { shallow } from 'enzyme';
+import moment from 'moment';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<MomentDisplay />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+
+
+describe('MomentDisplay positive cases', () => {
+
+  it('Rendering moment, will format it properly', () => {
+    
+    // Arrange
+    const date: moment.Moment = moment("20111031", "YYYYMMDD");
+
+    // Act
+    const wrapper = shallow(<MomentDisplay date={date}/>);
+
+    // Assert
+    const text = wrapper.text();
+    expect(text).toEqual('2011.10.31');
+  });
+
+
+  it('Empty parameter return nothing', () => {
+    
+    // Arrange
+
+    // Act
+    const wrapper = shallow(<MomentDisplay/>);
+
+    // Assert
+    expect(wrapper.text()).toEqual('');
+    expect(wrapper).toMatchObject({});
+  });
+
+})
