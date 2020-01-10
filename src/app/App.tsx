defb5322636cbbf198eea6e52abc9ddfda929dfd
Bankuti Adam <bankuti.adam@grape.hu>
2020-01-10 10:00:44 +0100

    Added antd locale support

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,15 +5,35 @@ import RouterView from 'app/router';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
+import { ConfigProvider } from 'antd';
+import huHU from 'antd/es/locale/hu_HU';
+import enGB from 'antd/es/locale/en_GB';
+import { useTranslation } from 'react-i18next';
 
-const App: React.FC = () => (
-  <BrowserRouter>
-    <Provider store={store}>
-      <ConnectedRouter history={history}>
-        <RouterView />
-      </ConnectedRouter>
-    </Provider>
-  </BrowserRouter>
-);
+const App: React.FC = () => {
+  const { i18n } = useTranslation();
+
+  const antLocale = () => {
+    console.log('antLocale');
+    switch (i18n.language) {
+      case 'en':
+        return enGB;
+      case 'hu':
+        return huHU;
+    }
+  };
+
+  return (
+    <BrowserRouter>
+      <ConfigProvider locale={antLocale()}>
+        <Provider store={store}>
+          <ConnectedRouter history={history}>
+            <RouterView />
+          </ConnectedRouter>
+        </Provider>
+      </ConfigProvider>
+    </BrowserRouter>
+  );
+};
 
 export default App;
