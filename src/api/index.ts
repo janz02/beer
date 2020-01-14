8bbb93896e520c0bf5af5e110ce40db18cb50a15
Albert Hajdu <hajdu.albert89@gmail.com>
2020-01-14 14:33:48 +0100

    Adding error logging and formatting fixes

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,13 +1,41 @@
-import { Configuration } from './swagger/runtime';
-import { CouponsApi, CategoriesApi, AuthApi } from './swagger/apis';
+import { Configuration } from './swagger/runtime'
+import { CouponsApi, CategoriesApi, AuthApi } from './swagger/apis'
+import { notification } from 'antd'
+import i18n from 'app/i18n'
+
+interface RequestError {
+  Code?: number;
+  ErrorKey?: string;
+  Message?: string;
+  Guid?: string;
+  StackTrace?: string;
+}
 
 const config: Configuration = new Configuration({
   basePath: process.env.REACT_APP_API_URL,
   apiKey: () => `Bearer ${sessionStorage.getItem('jwt')}`,
-});
+  middleware: [
+    {
+      post: ctx => {
+        if (ctx.response.status >= 400) {
+          ctx.response.json().then((x: RequestError) => {
+            notification.error({
+              message: i18n.t(x.ErrorKey!),
+              description: x.Guid,
+              duration: null
+            })
+            console.table({ ...x, url: ctx.url })
+          })
+        }
+
+        return Promise.resolve()
+      }
+    }
+  ]
+})
 
 export default {
   coupons: new CouponsApi(config),
   categories: new CategoriesApi(config),
   auth: new AuthApi(config)
-};
+}
