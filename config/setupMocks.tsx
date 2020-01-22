5c5cc8f05c7031e7488592a2d9a15ae0afcbe8d8
Kovács Ádám <kovacs.adam@grape.hu>
2020-01-22 17:58:56 +0100

    Fix tests

---
+++ config/setupMocks.tsx
@@ -0,0 +1,25 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import configureStore from 'redux-mock-store'
+import * as ReactReduxHooks from '../src/hooks/react-redux-hooks'
+import thunk from 'redux-thunk'
+import React from 'react'
+
+/**
+ * Use this, if you want to check whether a certain amount of sub-component is loaded
+ * Eg. a foreach renders 4 Button
+ *
+ * This will not care about useEffect, and cannot push buttons
+ * Optimal for simple logic checks
+ * @param children The Component which it needs to be rendered
+ * @param store An object, that represents the Redux store. Just simply pass an object, that the useSelector will query like: {auth: { loggedIn: true }}
+ */
+export const setupMocks = (store: {} = {}): void => {
+  const mockStore = configureStore([thunk])(store)
+
+  const useEffect = jest.spyOn(React, 'useEffect')
+  useEffect.mockImplementationOnce((f: any) => f())
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  jest.spyOn(ReactReduxHooks, 'useSelector').mockImplementation(state => mockStore.getState())
+  jest.spyOn(ReactReduxHooks, 'useDispatch').mockImplementation(() => mockStore.dispatch)
+}
--- src/features/coupons/components/CouponEditorForm.test.tsx
+++ src/features/coupons/components/CouponEditorForm.test.tsx
@@ -2,44 +2,25 @@
 import React from 'react'
 import { CouponEditorForm } from './CouponEditorForm'
 import { Card } from 'antd'
-import configureStore from 'redux-mock-store'
-import * as ReactReduxHooks from 'hooks/react-redux-hooks'
 import * as ReactRouterDom from 'react-router-dom'
+import { setupMocks } from '../../../../config/setupMocks'
 
-import thunk from 'redux-thunk'
 import { shallow } from 'enzyme'
 
 describe('CouponEditorForm tests', () => {
-  let useEffect: any
-  let store: any
-
-  const mockUseEffect = (): any => {
-    useEffect.mockImplementationOnce((f: any) => f())
-  }
-
-  beforeEach(() => {
-    store = configureStore([thunk])({ coupons: { categories: [] } })
-
-    useEffect = jest.spyOn(React, 'useEffect')
-    mockUseEffect()
-
-    jest.spyOn(ReactReduxHooks, 'useSelector').mockImplementation(state => store.getState())
-
-    jest.spyOn(ReactReduxHooks, 'useDispatch').mockImplementation(() => store.dispatch)
-  })
-
   it('should not display button when not editing or not new', () => {
     // Arrange
     const props = {
-      handleCouponSave: (v: any) => {
+      handleCouponSave: () => {
         /* no implementation */
       },
       loading: false,
       couponIsNew: false
     }
-    const state = { coupons: { categories: [] } }
 
-    // // Act
+    setupMocks({ coupons: { categories: [] } })
+
+    // Act
     const wrapper = shallow(
       <ReactRouterDom.BrowserRouter>
         <CouponEditorForm {...props} />
