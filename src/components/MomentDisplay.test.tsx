e1159b7876272373a07e0a566c6dd69cc8eb81f8
Albert Hajdu <hajdu.albert89@gmail.com>
2020-01-22 12:41:27 +0100

    Fixing tests

--- src/components/MomentDisplay.test.tsx
+++ src/components/MomentDisplay.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import MomentDisplay from './MomentDisplay'
+import { MomentDisplay } from './MomentDisplay'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
