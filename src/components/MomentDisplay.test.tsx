5bc0bd2636a4690cba0eb23f4cafc5753881091e
Kovács Ádám <kovacs.adam@grape.hu>
2020-01-22 16:22:48 +0100

    Fix tests

--- src/components/MomentDisplay.test.tsx
+++ src/components/MomentDisplay.test.tsx
@@ -1,34 +1,29 @@
 import React from 'react'
 import { MomentDisplay } from './MomentDisplay'
-import { shallow } from 'enzyme';
-import moment from 'moment';
+import { shallow } from 'enzyme'
+import moment from 'moment'
 
 describe('MomentDisplay tests', () => {
-
   it('should render a formatted date when props is given', () => {
-    
     // Arrange
-    const date: moment.Moment = moment("20111031", "YYYYMMDD");
+    const date: moment.Moment = moment('20111031', 'YYYYMMDD')
 
     // Act
-    const wrapper = shallow(<MomentDisplay date={date}/>);
+    const wrapper = shallow(<MomentDisplay date={date} />)
 
     // Assert
-    const text = wrapper.text();
-    expect(text).toEqual('2011.10.31');
-  });
-
+    const text = wrapper.text()
+    expect(text).toEqual('2011.10.31')
+  })
 
   it('should render nothing when props is empty', () => {
-    
     // Arrange
 
     // Act
-    const wrapper = shallow(<MomentDisplay/>);
+    const wrapper = shallow(<MomentDisplay />)
 
     // Assert
-    expect(wrapper.text()).toEqual('');
-    expect(wrapper).toMatchObject({});
-  });
-
+    expect(wrapper.text()).toEqual('')
+    expect(wrapper).toMatchObject({})
+  })
 })
--- src/features/coupons/components/CouponEditorForm.test.tsx
+++ src/features/coupons/components/CouponEditorForm.test.tsx
@@ -1,61 +1,55 @@
-import React from 'react';
-import { CouponEditorForm } from './CouponEditorForm';
-import { Card } from 'antd';
-import configureStore from "redux-mock-store";
-import * as ReactReduxHooks from "hooks/react-redux-hooks";
-import * as ReactRouterDom from 'react-router-dom';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React from 'react'
+import { CouponEditorForm } from './CouponEditorForm'
+import { Card } from 'antd'
+import configureStore from 'redux-mock-store'
+import * as ReactReduxHooks from 'hooks/react-redux-hooks'
+import * as ReactRouterDom from 'react-router-dom'
 
-import thunk from "redux-thunk";
-import { shallow } from 'enzyme';
+import thunk from 'redux-thunk'
+import { shallow } from 'enzyme'
 
 describe('CouponEditorForm tests', () => {
-  let useEffect: any;
-  let store: any;
+  let useEffect: any
+  let store: any
 
-  const mockUseEffect = () => {
-    useEffect.mockImplementationOnce((f: any) => f());
-  };
+  const mockUseEffect = (): any => {
+    useEffect.mockImplementationOnce((f: any) => f())
+  }
 
   beforeEach(() => {
-    store = configureStore([thunk])({ coupons: { categories: [] } });
+    store = configureStore([thunk])({ coupons: { categories: [] } })
 
-    useEffect = jest.spyOn(React, "useEffect");
-    mockUseEffect();
-    mockUseEffect();
+    useEffect = jest.spyOn(React, 'useEffect')
+    mockUseEffect()
 
-    jest
-      .spyOn(ReactReduxHooks, "useSelector")
-      .mockImplementation(state => store.getState());
+    jest.spyOn(ReactReduxHooks, 'useSelector').mockImplementation(state => store.getState())
 
-    jest
-      .spyOn(ReactReduxHooks, "useDispatch")
-      .mockImplementation(() => store.dispatch);
-
-
-
-  });
-
+    jest.spyOn(ReactReduxHooks, 'useDispatch').mockImplementation(() => store.dispatch)
+  })
 
   it('should not display button when not editing or not new', () => {
-
     // Arrange
     const props = {
-      handleCouponSave: (v: any) => { },
+      handleCouponSave: (v: any) => {
+        /* no implementation */
+      },
       loading: false,
       couponIsNew: false
-    };
-    const state = { coupons: { categories: [] } };
+    }
+    const state = { coupons: { categories: [] } }
 
     // // Act
-    const wrapper = shallow(<ReactRouterDom.BrowserRouter>
-      <CouponEditorForm {...props} />
-    </ReactRouterDom.BrowserRouter>
-    );
+    const wrapper = shallow(
+      <ReactRouterDom.BrowserRouter>
+        <CouponEditorForm {...props} />
+      </ReactRouterDom.BrowserRouter>
+    )
 
-    // // Assert    
-    // const sut = wrapper.find(CouponEditorForm);
-    expect(wrapper.find(Card)).toBeTruthy();
-    // expect(sut.props().couponIsNew).toEqual(false);
-  });
+    // Assert
+    const sut = wrapper.find(CouponEditorForm)
+
+    expect(sut.find(Card)).toBeTruthy()
+    expect(sut.props().couponIsNew).toEqual(false)
+  })
 })
-
