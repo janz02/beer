f774a55bdf714f7bb410dcd92578f1737cefa7ca
Albert Hajdu <hajdu.albert89@gmail.com>
2020-02-10 14:41:08 +0100

    Refactoring pre middleware config

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,16 @@
-import { PartnerContactsApi } from './swagger/apis/PartnerContactsApi'
-import { CouponCommentsApi } from './swagger/apis/CouponCommentsApi'
 import { Configuration } from './swagger/runtime'
-import { CouponsApi, CategoriesApi, AuthApi, SitesApi, PartnersApi, TagsApi } from './swagger/apis'
+import {
+  CouponsApi,
+  CategoriesApi,
+  AuthApi,
+  SitesApi,
+  PartnersApi,
+  TagsApi,
+  CouponCommentsApi,
+  PartnerContactsApi
+} from './swagger/apis'
 import { notification } from 'antd'
 import i18n from 'app/i18n'
-import JwtDecode from 'jwt-decode'
 
 interface RequestError {
   Code?: number
@@ -16,9 +22,8 @@ interface RequestError {
 
 const getUrl = window.location
 const baseUrl = getUrl.protocol + '//' + getUrl.host + '/' + getUrl.pathname.split('/')[1]
-let tokenPromise: Promise<void> | null
 
-const config: Configuration = new Configuration({
+export const config: Configuration = new Configuration({
   basePath: process.env.REACT_APP_API_URL || baseUrl,
   apiKey: () => `Bearer ${sessionStorage.getItem('jwt')}`,
   middleware: [
@@ -59,45 +64,3 @@ export const api = {
   partner: new PartnersApi(config),
   partnerContacts: new PartnerContactsApi(config)
 }
-
-config.middleware.push(
-  {
-    pre: async ctx => {
-      if (!tokenPromise && !ctx.url.endsWith('Auth/Refresh') && sessionStorage.getItem('jwt')) {
-        const jwtExpiration = sessionStorage.getItem('jwtExpiration')
-        if (jwtExpiration && +jwtExpiration < new Date().getTime()) {
-          let refreshToken = sessionStorage.getItem('refreshToken')
-          if (refreshToken) {
-            tokenPromise = new Promise(resolve => {
-              api.auth.refresh({ refreshDto: { refreshToken: refreshToken } }).then(user => {
-                const jwt = user.jwtToken
-                refreshToken = user.refreshToken || ''
-                const decodedJwt: any = jwt && JwtDecode(jwt)
-                const jwtExpiration = decodedJwt?.exp
-
-                jwt && sessionStorage.setItem('jwt', jwt)
-                refreshToken && sessionStorage.setItem('refreshToken', refreshToken)
-                // Also correcting precision.
-                jwtExpiration && sessionStorage.setItem('jwtExpiration', `${jwtExpiration}000`)
-                resolve()
-                tokenPromise = null
-              })
-            })
-          }
-        }
-      }
-
-      return tokenPromise || Promise.resolve()
-    }
-  },
-  {
-    pre: async ctx => {
-      ctx.init.headers = {
-        ...ctx.init.headers,
-        Authorization: `Bearer ${sessionStorage.getItem('jwt')}`
-      }
-
-      return Promise.resolve()
-    }
-  }
-)
