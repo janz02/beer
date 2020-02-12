3ebbac796fcd933d672d4c83f58170943fb5a635
Bankuti Adam <bankuti.adam@grape.hu>
2020-02-12 13:56:48 +0100

    wip grapesjs: added indexxeddb

--- src/features/news-letter/NewsLetterEditor.tsx
+++ src/features/news-letter/NewsLetterEditor.tsx
@@ -1,25 +1,28 @@
 import React, { FC, useEffect } from 'react'
+import './NewsLetterEditor.scss'
 // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
 // @ts-ignore
 import grapesjs from 'grapesjs'
 // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
 // @ts-ignore
 import grapesjsNewsLetter from 'grapesjs-preset-newsletter'
-// import grapesjs from 'grapesjs'
-import './NewsLetterEditor.scss'
+// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+// @ts-ignore
+import grapesjsIndexxeddb from 'grapesjs-indexeddb'
 
 export const NewsLetterEditor: FC = () => {
   useEffect(() => {
-    const storagePrefix = 'gjs-pkm-'
+    const storagePrefix = 'gjs-pkm'
     setTimeout(() => {
       grapesjs.init({
         container: '#grapesjs',
         storageManager: {
+          type: 'indexeddb',
           id: storagePrefix
         },
         height: '100%',
-        plugins: [grapesjsNewsLetter],
-        components: '<article class="hello">Hello PKM K K user!</article>'
+        plugins: [grapesjsNewsLetter, grapesjsIndexxeddb],
+        components: '<article class="hello">Hello PKM user!</article>'
       })
     }, 0)
     return () => {
