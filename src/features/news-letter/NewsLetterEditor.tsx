5dfc0293f1ba606818d6f7272ee4bd415cdfb13c
Bankuti Adam <bankuti.adam@grape.hu>
2020-02-20 12:21:18 +0100

    Grapesjs translation

--- src/features/news-letter/NewsLetterEditor.tsx
+++ src/features/news-letter/NewsLetterEditor.tsx
@@ -1,5 +1,7 @@
-import React, { FC, useEffect, useRef } from 'react'
+import React, { FC, useEffect, useRef, useState } from 'react'
 import './NewsLetterEditor.scss'
+import { useTranslation } from 'react-i18next'
+import locale from './locale/'
 // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
 // @ts-ignore
 import grapesjs from 'grapesjs'
@@ -9,8 +11,11 @@ import grapesjsNewsLetter from 'grapesjs-preset-newsletter'
 // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
 // @ts-ignore
 import grapesjsIndexxeddb from 'grapesjs-indexeddb'
-import { Button } from 'antd'
-import { useTranslation } from 'react-i18next'
+
+enum CMD {
+  // used as locale key and cmd string
+  SaveTemplate = 'pkm-save-template'
+}
 
 export interface NewsLetterTemplate {
   html?: string | null
@@ -33,57 +38,68 @@ export interface NewsLetterEditorProps {
 export const NewsLetterEditor: FC<NewsLetterEditorProps> = props => {
   const template = props?.template ?? defaultTemplate
 
-  const { t } = useTranslation()
+  const { t, i18n } = useTranslation()
 
   const editor = useRef<any>()
 
   const saveTemplate = (newTemplate: NewsLetterTemplate): void => {
-    console.log({ newTemplate, editor: editor.current, cmd: editor.current.Commands.getAll() })
+    console.log({
+      newTemplate,
+      editor: editor.current,
+      cmd: editor.current.Commands.getAll(),
+      I18n: editor.current.I18n
+    })
   }
 
   useEffect(() => {
     const storagePrefix = 'gjs-pkm'
+    const translations = { en: (locale as any)[i18n.language] }
 
-    if (!editor.current) {
-      indexedDB.deleteDatabase(storagePrefix)
-      editor.current = grapesjs.init({
-        container: '#grapesjs',
-        storageManager: {
-          type: 'indexeddb',
-          id: `${storagePrefix}-`
+    editor.current = grapesjs.init({
+      // TODO: consider removing db, giving db id
+      container: '#grapesjs',
+      storageManager: {
+        type: 'indexeddb',
+        id: `${storagePrefix}-`
+      },
+      height: '100%',
+      plugins: [grapesjsNewsLetter, grapesjsIndexxeddb],
+      pluginsOpts: {
+        [grapesjsIndexxeddb]: {
+          dbName: storagePrefix
         },
-        height: '100%',
-        plugins: [grapesjsNewsLetter, grapesjsIndexxeddb],
-        pluginsOpts: {
-          [grapesjsIndexxeddb]: {
-            dbName: storagePrefix
-          }
+        [grapesjsNewsLetter]: {
+          // Translations that are not handled by the news letter preset.
+          // Update only the locale files, the `en` is a placeholder for all translations.
+          ...translations.en?.preset
         }
-      })
-      editor.current.Commands.add('save-template', {
-        run: (editor: any, sender: any) => {
-          const html = editor.getHtml()
-          const css = editor.getCss()
-          saveTemplate({ html, css })
+      },
+      i18n: {
+        // bugfix: The editor.current.I18n.setLocale function is not switching the languege.
+        // The locale is kept always as `en` but the translations are switched,
+        // and the the editor is reinitialized.
+        locale: 'en',
+        localeFallback: 'en',
+        messages: translations
+      }
+    })
+    editor.current.Commands.add(CMD.SaveTemplate, {
+      run: (editor: any, sender: any) => {
+        const html = editor.getHtml()
+        const css = editor.getCss()
+        saveTemplate({ html, css })
+      }
+    })
+    editor.current.Panels.addButton('options', [
+      {
+        id: CMD.SaveTemplate,
+        className: 'fa fa-save',
+        command: (editor: any, sender: any) => {
+          editor.runCommand(CMD.SaveTemplate)
         }
-      })
-      editor.current.Panels.addButton('options', [
-        {
-          id: 'save-template',
-          className: 'fa fa-save',
-          attributes: {
-            title: t('common.save')
-          },
-          command: (editor: any, sender: any) => {
-            editor.runCommand('save-template')
-          }
-        }
-      ])
-    }
-    return () => {
-      indexedDB.deleteDatabase(storagePrefix)
-    }
-  }, [t])
+      }
+    ])
+  }, [i18n.language, t])
 
   useEffect(() => {
     if (editor.current) {
