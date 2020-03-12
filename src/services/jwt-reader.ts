d660443fbacf73ea47292a4666e6ca5cdc05486d
Bankuti Adam <bankuti.adam@grape.hu>
2020-03-12 13:21:24 +0100

    Jwt role detector fix

--- src/services/jwt-reader.ts
+++ src/services/jwt-reader.ts
@@ -2,17 +2,19 @@ import JwtDecode from 'jwt-decode'
 import { UserData } from 'models/user'
 import { Roles } from 'api/swagger/models'
 
-const formatRoles = (rawRules: string): Roles[] => {
-  return rawRules ? (rawRules.split(',') as Roles[]) : []
+const ROLE_KEY = 'http://schemas.microsoft.com/ws/2008/06/identity/claims/role'
+
+const formatRoles = (jwt: any): Roles[] => {
+  const rawRoles = jwt?.[ROLE_KEY]
+  return rawRoles ? [].concat(rawRoles) : []
 }
 
-export const getJwtUserdata = (token?: string | null): UserData => {
-  const jwt = token ?? sessionStorage.getItem('jwt')
+export const getJwtUserdata = (token?: string[] | string | null): UserData => {
+  const jwt: any = token ?? sessionStorage.getItem('jwt')
   const decodedJwt: any = jwt && JwtDecode(jwt)
-
   const user: UserData = {
     email: decodedJwt?.email,
-    roles: formatRoles(decodedJwt?.roles),
+    roles: formatRoles(decodedJwt),
     exp: decodedJwt?.exp
   }
   return user
