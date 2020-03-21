8d9454836b5289b3336a11fa3e832aa4699013a5
Kovács Ádám <kovacs.adam@grape.hu>
2020-03-21 11:16:57 +0100

    Language selector placed in menu

--- src/components/LanguageSelector.tsx
+++ src/components/LanguageSelector.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import EnLogo from 'assets/img/flags/en.svg'
 import HuLogo from 'assets/img/flags/hu.svg'
-import { Dropdown, Menu } from 'antd'
-import { DownOutlined } from '@ant-design/icons'
+import { Menu } from 'antd'
 import { useTranslation } from 'react-i18next'
 import './LanguageSelector.scss'
 import { setMomentLocale } from 'app/i18n/moment-locale'
+import SubMenu from 'antd/lib/menu/SubMenu'
 
 export const LanguageSelector: React.FC = () => {
   const { t, i18n } = useTranslation()
@@ -29,21 +29,19 @@ export const LanguageSelector: React.FC = () => {
     }
   ]
 
-  const languageOptions = (
-    <Menu key="language" className="language-selector-dropdown-content">
-      {availableLanguages.map(({ image, key, label }) => (
-        <Menu.Item key={key} onClick={() => changeLanguage(key)}>
-          <img
-            src={image}
-            alt={label}
-            title={label}
-            className="language-selector-dropdown-content__image"
-          />
-          <span className="language-selector-dropdown-content__text">{label}</span>
-        </Menu.Item>
-      ))}
-    </Menu>
-  )
+  const languageOptions = (): JSX.Element[] => {
+    return availableLanguages.map(({ image, key, label }) => (
+      <Menu.Item key={key} onClick={() => changeLanguage(key)}>
+        <img
+          src={image}
+          alt={label}
+          title={label}
+          className="language-selector-dropdown-content__image"
+        />
+        <span className="language-selector-dropdown-content__text">{label}</span>
+      </Menu.Item>
+    ))
+  }
 
   const CurrentLanguageImg = (): JSX.Element | null => {
     const currentLanguage = availableLanguages.find(({ key }) => key === i18n.language)
@@ -63,12 +61,23 @@ export const LanguageSelector: React.FC = () => {
   }
 
   return (
-    <Dropdown className="language-selector" overlay={languageOptions} trigger={['click']}>
-      <div>
-        <CurrentLanguageImg />
-        <span className="language-selector__text">{t('languages.language')}</span>
-        <DownOutlined />
-      </div>
-    </Dropdown>
+    <Menu
+      theme="dark"
+      selectable={false}
+      key="language"
+      className="language-selector language-selector-dropdown-content"
+    >
+      <SubMenu
+        key="language"
+        title={
+          <div>
+            <CurrentLanguageImg />
+            <span className="language-selector__text">{t('languages.language')}</span>
+          </div>
+        }
+      >
+        {languageOptions()}
+      </SubMenu>
+    </Menu>
   )
 }
