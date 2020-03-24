b25854d8ee917374ce99057162f10b0e728e1843
Kovács Ádám <kovacs.adam@grape.hu>
2020-03-24 15:59:56 +0100

    Revert "Revert "Merge branch 'feature/NRMRTDKPR-597-kampany-szerkeszto-sitebuild' into feature/NRMRTDKPR-538-Kampany-listazo""
    This reverts commit 7fada8b8523315d0644718b548104f5982039da7.
    
    # Conflicts:
    #	src/api/swagger.json
    #	src/app/i18n/locales/en.json
    #	src/app/i18n/locales/hu.json

--- src/hooks/useCommonFormRules.ts
+++ src/hooks/useCommonFormRules.ts
@@ -87,5 +87,18 @@ export function useCommonFormRules() {
     [t]
   )
 
-  return { required, requiredString, password, number, email, max }
+  /**
+   * Integer
+   * @param message (optional) string
+   */
+  const positiveInteger = useCallback(
+    (message?: string): Rule => ({
+      min: 1,
+      type: 'integer',
+      message: message || t('error.common.number-must-be-positive-integer')
+    }),
+    [t]
+  )
+
+  return { required, requiredString, password, number, email, max, positiveInteger }
 }
--- src/router/router.tsx
+++ src/router/router.tsx
@@ -25,7 +25,7 @@ import { PartnerListPage } from 'features/partners/partnerList/PartnerListPage'
 import { SelfPartnerEditorPage } from 'features/partners/selfPartner/SelfPartnerEditorPage'
 import { PartnerEditorPage } from 'features/partners/partnerEditor/PartnerEditorPage'
 
-const comboRoles = {
+export const comboRoles = {
   forPartner: [Roles.PartnerContactApprover, Roles.PartnerContactEditor],
   forNkm: [
     Roles.Administrator,
