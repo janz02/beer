2e6c5af1b5178c489f5a7cea8bab36d1a19c6808
Albert Hajdu <hajdu.albert89@gmail.com>
2020-03-26 11:31:26 +0100

    More momentdisplay tests

--- src/components/MomentDisplay.test.tsx
+++ src/components/MomentDisplay.test.tsx
@@ -2,8 +2,13 @@ import React from 'react'
 import { MomentDisplay } from './MomentDisplay'
 import { shallow } from 'enzyme'
 import moment from 'moment'
+import { setMomentLocale } from 'app/i18n/moment-locale'
 
-describe('MomentDisplay tests', () => {
+describe('MomentDisplay hu tests', () => {
+  beforeAll(() => {
+    setMomentLocale('hu')
+  })
+
   it('should render a formatted date by default', () => {
     // Arrange
     const date = moment(new Date(2011, 9, 31, 10, 30))
@@ -13,7 +18,7 @@ describe('MomentDisplay tests', () => {
 
     // Assert
     const text = wrapper.text()
-    expect(text).toEqual('2011.10.31')
+    expect(text).toEqual('2011.10.31.')
   })
 
   it('should render a formatted date and time when mode is date time', () => {
@@ -26,7 +31,7 @@ describe('MomentDisplay tests', () => {
 
     // Assert
     const text = wrapper.text()
-    expect(text).toEqual('2011.10.31 10:30')
+    expect(text).toEqual('2011. október 31. 10:30')
   })
 
   it('should render a formatted time when mode is time', () => {
@@ -48,11 +53,100 @@ describe('MomentDisplay tests', () => {
     const mode = 'date/time'
 
     // Act
+
+    const wrapper = shallow(<MomentDisplay date={date} mode={mode} />)
+
+    const divs = wrapper.find('div')
+    const firstDivText = divs
+      .children()
+      .at(0)
+      .text()
+    const secondDivText = divs
+      .children()
+      .at(1)
+      .text()
+
+    // Assert
+    expect(firstDivText).toEqual('2011.10.31.')
+    expect(secondDivText).toEqual('10:30')
+  })
+
+  it('should render nothing when props is empty', () => {
+    // Arrange
+
+    // Act
+    const wrapper = shallow(<MomentDisplay />)
+
+    // Assert
+    expect(wrapper.text()).toEqual('')
+    expect(wrapper).toMatchObject({})
+  })
+})
+
+describe('MomentDisplay en tests', () => {
+  beforeAll(() => {
+    setMomentLocale('en')
+  })
+
+  it('should render a formatted date by default', () => {
+    // Arrange
+    const date = moment(new Date(2011, 9, 31, 10, 30))
+
+    // Act
+    const wrapper = shallow(<MomentDisplay date={date} />)
+
+    // Assert
+    const text = wrapper.text()
+    expect(text).toEqual('10/31/2011')
+  })
+
+  it('should render a formatted date and time when mode is date time', () => {
+    // Arrange
+    const date = moment(new Date(2011, 9, 31, 10, 30))
+    const mode = 'date time'
+
+    // Act
     const wrapper = shallow(<MomentDisplay date={date} mode={mode} />)
 
     // Assert
     const text = wrapper.text()
-    expect(text).toEqual('2011.10.3110:30')
+    expect(text).toEqual('October 31, 2011 10:30 AM')
+  })
+
+  it('should render a formatted time when mode is time', () => {
+    // Arrange
+    const date = moment(new Date(2011, 9, 31, 10, 30))
+    const mode = 'time'
+
+    // Act
+    const wrapper = shallow(<MomentDisplay date={date} mode={mode} />)
+
+    // Assert
+    const text = wrapper.text()
+    expect(text).toEqual('10:30 AM')
+  })
+
+  it('should render a formatted date and time in divs when mode is date/time', () => {
+    // Arrange
+    const date = moment(new Date(2011, 9, 31, 10, 30))
+    const mode = 'date/time'
+
+    // Act
+    const wrapper = shallow(<MomentDisplay date={date} mode={mode} />)
+
+    const divs = wrapper.find('div')
+    const firstDivText = divs
+      .children()
+      .at(0)
+      .text()
+    const secondDivText = divs
+      .children()
+      .at(1)
+      .text()
+
+    // Assert
+    expect(firstDivText).toEqual('10/31/2011')
+    expect(secondDivText).toEqual('10:30 AM')
   })
 
   it('should render nothing when props is empty', () => {
