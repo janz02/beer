ee7bda792e8ee1a019a64344cd9e0074a68b1202
Bankuti Adam <bankuti.adam@grape.hu>
2020-04-16 19:20:44 +0200

    Added new 'from now' mode to momentDisplay

--- src/components/MomentDisplay.tsx
+++ src/components/MomentDisplay.tsx
@@ -3,7 +3,7 @@ import moment from 'moment'
 
 interface MomentDisplayProps {
   date?: moment.Moment
-  mode?: 'date' | 'time' | 'date time' | 'date/time'
+  mode?: 'date' | 'time' | 'date time' | 'date/time' | 'from now'
 }
 
 export const DATE_FORMAT = 'L'
@@ -11,19 +11,25 @@ export const TIME_FORMAT = 'LT'
 
 export const MomentDisplay: React.FC<MomentDisplayProps> = props => {
   const { date, mode } = props
+
   if (!date) return <></>
 
-  if (mode === 'time') {
-    return <>{date.format(TIME_FORMAT)}</>
-  } else if (mode === 'date time') {
-    return <>{`${date.format(DATE_FORMAT)} ${date.format(TIME_FORMAT)}`}</>
-  } else if (mode === 'date/time') {
-    return (
-      <>
-        <div>{date.format(DATE_FORMAT)}</div>
-        <div>{date.format(TIME_FORMAT)}</div>
-      </>
-    )
+  switch (mode) {
+    case 'time':
+      return <>{date.format(TIME_FORMAT)}</>
+    case 'from now':
+      return <>{date.fromNow()}</>
+    case 'date time':
+      return <>{`${date.format(DATE_FORMAT)} ${date.format(TIME_FORMAT)}`}</>
+    case 'date/time':
+      return (
+        <>
+          <div>{date.format(DATE_FORMAT)}</div>
+          <div>{date.format(TIME_FORMAT)}</div>
+        </>
+      )
+    case 'date':
+    default:
+      return <>{date.format(DATE_FORMAT)}</>
   }
-  return <>{date.format(DATE_FORMAT)}</>
 }
