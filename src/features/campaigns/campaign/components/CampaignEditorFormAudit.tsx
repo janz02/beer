e813c9eafa704a0ac3af42f930582a378116f226
Albert Hajdu <hajdu.albert89@gmail.com>
2020-04-22 11:14:22 +0200

    Form sections refactor

---
+++ src/features/campaigns/campaign/components/CampaignEditorFormAudit.tsx
@@ -0,0 +1,47 @@
+import React, { FC } from 'react'
+import { Form, Row, Col } from 'antd'
+import { useTranslation } from 'react-i18next'
+import { MomentDisplay } from 'components/MomentDisplay'
+import { useCampaign } from '../useCampaign'
+
+export const CampaignEditorFormAudit: FC = () => {
+  const { t } = useTranslation()
+  const { coupon, rowGutter } = useCampaign()
+
+  return (
+    <Row gutter={rowGutter}>
+      <Col span={8}>
+        <Form.Item label={t('coupon-create.field.created')}>
+          {coupon?.createdBy && (
+            <div>
+              {`${coupon?.createdBy}, `}
+              <MomentDisplay date={coupon?.createdDate} />
+            </div>
+          )}
+        </Form.Item>
+      </Col>
+
+      <Col span={8}>
+        <Form.Item label={t('coupon-create.field.modified')}>
+          {coupon?.modifiedBy && (
+            <div>
+              {`${coupon?.modifiedBy}, `}
+              <MomentDisplay date={coupon?.modifiedDate} />
+            </div>
+          )}
+        </Form.Item>
+      </Col>
+
+      <Col span={8}>
+        <Form.Item label={t('coupon-create.field.approved')}>
+          {coupon?.approvedBy && (
+            <div>
+              {`${coupon?.approvedBy}, `}
+              <MomentDisplay date={coupon?.approvedDate} />
+            </div>
+          )}
+        </Form.Item>
+      </Col>
+    </Row>
+  )
+}
