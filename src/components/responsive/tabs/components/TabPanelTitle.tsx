12912494a1e383c37d4d7097deda9f032d031e32
Nyiko Zoltan <nyikozoli@gmail.com>
2020-09-07 11:43:53 +0200

    Add responsive tabs comp.

---
+++ src/components/responsive/tabs/components/TabPanelTitle.tsx
@@ -0,0 +1,27 @@
+import React, { FC, useMemo } from 'react'
+import Badge, { BadgeProps } from 'antd/lib/badge'
+import { useIsMediumScreen } from 'hooks'
+
+interface TabPanelTitleProps {
+  title: string
+  icon?: React.ReactNode
+  badgeProps?: BadgeProps
+}
+
+export const TabPanelTitle: FC<TabPanelTitleProps> = ({ title, icon, badgeProps }) => {
+  const isMediumScreen = useIsMediumScreen()
+
+  const hasToDisplayTitle = useMemo(() => !isMediumScreen || !icon, [isMediumScreen, icon])
+
+  const panelTitle = useMemo(
+    () => (
+      <span className="tab-panel-title">
+        {icon}
+        {hasToDisplayTitle && title}
+      </span>
+    ),
+    [hasToDisplayTitle, icon, title]
+  )
+
+  return badgeProps ? <Badge {...badgeProps}>{panelTitle}</Badge> : panelTitle
+}
---
+++ src/components/responsive/tabs/index.tsx
@@ -0,0 +1,7 @@
+import { ResponsiveTabs } from './ResponsiveTabs'
+import { TabPanelTitle } from './components/TabPanelTitle'
+import { Tabs } from 'antd'
+
+const { TabPane } = Tabs
+
+export { ResponsiveTabs, TabPanelTitle, TabPane }
