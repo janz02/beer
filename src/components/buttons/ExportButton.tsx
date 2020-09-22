d8aa03200e529ff5e199ddb997d8f09d045dd595
Nyiko Zoltan <nyikozoli@gmail.com>
2020-09-22 16:11:21 +0200

    Add export button as comp.

--- src/components/buttons/ExportButton.tsx
+++ src/components/buttons/ExportButton.tsx
@@ -2,11 +2,22 @@ import React, { FC } from 'react'
 import { Button } from 'antd'
 import { ButtonProps } from 'antd/lib/button'
 import { DownloadOutlined } from '@ant-design/icons'
+import { useTranslation } from 'react-i18next'
+import { hasPermission } from 'services/jwt-reader'
 
 export const ExportButton: FC<ButtonProps> = ({ children, ...props }) => {
-  return (
-    <Button type="primary" icon={<DownloadOutlined />} size="large" {...props}>
+  const { t } = useTranslation()
+
+  // TODO check export permissions
+  return hasPermission() ? (
+    <Button
+      type="primary"
+      icon={<DownloadOutlined />}
+      size="small"
+      title={t('common.export')}
+      {...props}
+    >
       {children}
     </Button>
-  )
+  ) : null
 }
