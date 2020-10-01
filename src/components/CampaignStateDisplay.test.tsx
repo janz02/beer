e327b3371b458f29fb88eaafe8eed37d9f07e638
AndrewJ <janek.andras@grape,hu>
2020-10-01 11:04:36 +0200

    feature campaign type + unit tests

---
+++ src/components/CampaignStateDisplay.test.tsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import { render } from 'enzyme'
+import { CampaignStateDisplay } from './CampaignStateDisplay'
+import { CouponState } from 'api/swagger/coupon'
+
+describe('CampaignStateDisplay tests', () => {
+  it('should render accepted state', () => {
+    // Arrange
+    const state: CouponState = CouponState.Accepted
+
+    // Act
+    const wrapper = render(<CampaignStateDisplay state={state} />)
+
+    // Assert
+    const display = wrapper.text()
+    expect(display).toEqual('coupon.state.accepted')
+  })
+
+  it('should render archived state', () => {
+    // Arrange
+    const state: CouponState = CouponState.Archived
+
+    // Act
+    const wrapper = render(<CampaignStateDisplay state={state} />)
+
+    // Assert
+    const display = wrapper.text()
+    expect(display).toEqual('coupon.state.archived')
+  })
+
+  it('should render closed state', () => {
+    // Arrange
+    const state: CouponState = CouponState.Closed
+
+    // Act
+    const wrapper = render(<CampaignStateDisplay state={state} />)
+
+    // Assert
+    const display = wrapper.text()
+    expect(display).toEqual('coupon.state.closed')
+  })
+
+  it('should render created state', () => {
+    // Arrange
+    const state: CouponState = CouponState.Created
+
+    // Act
+    const wrapper = render(<CampaignStateDisplay state={state} />)
+
+    // Assert
+    const display = wrapper.text()
+    expect(display).toEqual('coupon.state.created')
+  })
+
+  it('should render waiting state', () => {
+    // Arrange
+    const state: CouponState = CouponState.Waiting
+
+    // Act
+    const wrapper = render(<CampaignStateDisplay state={state} />)
+
+    // Assert
+    const display = wrapper.text()
+    expect(display).toEqual('coupon.state.waiting')
+  })
+})
