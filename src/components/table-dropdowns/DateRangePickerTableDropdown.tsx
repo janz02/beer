7c233e1a7f0cb842484fc0da60e75a53ead60be3
AndrewJ <janek.andras@grape,hu>
2020-10-07 09:26:58 +0200

    fix unit test fix

--- src/components/table-dropdowns/DateRangePickerTableDropdown.tsx
+++ src/components/table-dropdowns/DateRangePickerTableDropdown.tsx
@@ -3,7 +3,6 @@ import { FilterDropdownProps } from 'antd/lib/table/interface'
 import { DatePicker } from 'antd'
 import { TableDropdownFooter } from './TableDropdownFooter'
 import './DateRangePickerTableDropdown.scss'
-const { RangePicker } = DatePicker
 
 export const DateRangePickerTableDropdown: FC<FilterDropdownProps> = props => {
   const { setSelectedKeys, selectedKeys } = props
@@ -11,7 +10,7 @@ export const DateRangePickerTableDropdown: FC<FilterDropdownProps> = props => {
   return (
     <div className="rangepicker-table-dropdown">
       <div className="date-input-container">
-        <RangePicker
+        <DatePicker.RangePicker
           onChange={(e: any) => setSelectedKeys([e || undefined])}
           value={(selectedKeys[0] as any) || null}
         />
