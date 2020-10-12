298b0216e5b5bcdd4e203b886a83df12fa813f5b
Niki <pillarz.nikolett@grape.hu>
2020-10-12 16:16:27 +0200

    BE keys generation added part 1 (incomplete check method)

--- config/generate-backend-translation-keys.js
+++ config/generate-backend-translation-keys.js
@@ -8,6 +8,7 @@ const https = require("https");
 // 1. Translation key list files
 const fileNameError = "src/app/i18n/backend-generated-error-codes.ts";
 const fileNameEnum = "src/app/i18n/backend-generated-enum-codes.ts";
+const fileNameKey = "src/app/i18n/backend-generated-key-codes.ts";
 
 // 2. Remove old list file and prepare the new one
 (() => {
@@ -21,7 +22,8 @@ const fileNameEnum = "src/app/i18n/backend-generated-enum-codes.ts";
 
 // Function to generate translation keys
 const generateKeys = (path, fileName, prefix = "", onData) => {
-  const req = https.request({
+  const req = https.request(
+    {
       hostname: "pkm-coupon-dev.grapetest.xyz",
       path,
       method: "GET"
@@ -52,16 +54,38 @@ const generateKeys = (path, fileName, prefix = "", onData) => {
 };
 
 // Check method for notification links
-const checkNotificationLinks = (json) => {
-  const notificationLinksRaw = fs.readFileSync('src/features/notification/notificationLinks.json');
+const checkNotificationLinks = json => {
+  const notificationLinksRaw = fs.readFileSync(
+    "src/features/notification/notificationLinks.json"
+  );
   const notificationLinks = JSON.parse(notificationLinksRaw);
 
-  const allNotificationHasLink = json.every(x => notificationLinks.filter(z => z.type === x).length === 1);
+  const allNotificationHasLink = json.every(
+    x => notificationLinks.filter(z => z.type === x).length === 1
+  );
 
   if (!allNotificationHasLink) {
-    throw Error("Notification links are missing for the newly added notifications. Check the feature/notification/README file");
+    throw Error(
+      "Notification links are missing for the newly added notifications. Check the feature/notification/README file"
+    );
   }
-}
+};
+
+const checkSystemParamKeys = json => {
+  const systemParamsRaw = fs.readFileSync(
+    "src/features/settings/systemParams/systemParams.json"
+  );
+  const systemParams = JSON.parse(systemParamsRaw);
+  const allSystemParamHasDescription = json.every(key =>
+    systemParams.find(frontendParam => frontendParam.key === key)
+  );
+
+  if (!allSystemParamHasDescription) {
+    throw Error(
+      "System parameter descriptions are missing for the newly added system parameter keys."
+    );
+  }
+};
 
 // 3. Generate lists
 generateKeys("/api/Information", fileNameError);
@@ -70,6 +94,12 @@ generateKeys(
   fileNameEnum,
   "enum.notification-type.",
   checkNotificationLinks
+);
+generateKeys(
+  "/api/SystemParameters/Keys",
+  fileNameKey,
+  "system-params.key.",
+  checkSystemParamKeys
 );
 
 shell.exec("npm run i18n:generate");
