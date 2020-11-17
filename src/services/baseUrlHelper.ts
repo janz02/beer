166cbe1d8b0d37e26802c79c914876589a8e629d
Niki <pillarz.nikolett@grape.hu>
2020-11-17 15:51:12 +0100

    simplified

--- src/services/baseUrlHelper.ts
+++ src/services/baseUrlHelper.ts
@@ -2,21 +2,7 @@
  * Returns the base url of the app
  */
 export function getUrl(appendUrl?: string): string {
-  const { origin } = window.location
-  let apiUrl = ''
-
-  switch (process.env.REACT_APP_MODE) {
-    case 'development':
-      apiUrl = `${process.env.REACT_APP_API_URL}${appendUrl}`
-      break
-    case 'production':
-      apiUrl = `${origin}${appendUrl}`
-      break
-    default:
-    case 'local':
-      apiUrl = `${appendUrl}`
-      break
-  }
-
-  return apiUrl
+  return process.env.REACT_APP_MODE === 'development'
+    ? `${process.env.REACT_APP_API_URL}${appendUrl}`
+    : `${appendUrl}`
 }
